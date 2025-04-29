bd724a5404866b620f1c24503d3aa5dd9edc86c5
Wender Machado <devwendermachado@gmail.com>
2025-04-29 11:30:15 -0300

    adiciona feature de keypress e de click, tratando a inserção de valores na tela

---
+++ index.js
@@ -0,0 +1,41 @@
+const main = document.querySelector('main')
+const root = document.querySelector(':root')
+const input = document.querySelector('input')
+const result = document.getElementById('result')
+
+
+const allowedKeys = ["(", ")", "/", "*", "-", "+", "9", "8", "7", "6", "5", "4", "3", "2", "1", "0", ".", "%", " "]
+
+document.querySelectorAll('.charKey').forEach((charKeyBtn)=>{
+  charKeyBtn.addEventListener('click', ()=> {
+    const value = charKeyBtn.dataset.value
+    input.value += value
+  })
+})
+
+document.getElementById('clear').addEventListener('click', ()=>{
+input.value = ''
+input.focus()
+})
+document.getElementById('equal').addEventListener('click', calculate)
+
+
+
+input.addEventListener('keydown', (ev)=>{
+  ev.preventDefault()
+
+  if(allowedKeys.includes(ev.key)){
+    input.value += ev.key
+    return
+  }
+  if(ev.key === "Backspace"){
+    input.value = input.value.slice(0, -1)
+  }
+  if(ev.key === "Enter"){
+   calculate()
+  }
+})
+
+function calculate(){
+  console.log('calculado')
+}
